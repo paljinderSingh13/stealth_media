41f158f6a2c873ab4ca293e4cd6146c66a684411
unknown <harshdeep66@gmail.com>
2020-08-25 09:51:59 +0530

    POS Setup with Laravel

---
+++ resources/views/posdev/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <title>POS</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+      <link rel="stylesheet" href="font/iconsmind-s/css/iconsminds.css" />
+      <link rel="stylesheet" href="font/simple-line-icons/css/simple-line-icons.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="css/vendor/bootstrap.min.css" />
+      <link rel="stylesheet" href="css/vendor/bootstrap.rtl.only.min.css" />
+      <link rel="stylesheet" href="css/vendor/fullcalendar.min.css" />
+      <link rel="stylesheet" href="css/vendor/dataTables.bootstrap4.min.css" />
+      <link rel="stylesheet" href="css/vendor/datatables.responsive.bootstrap4.min.css" />
+      <link rel="stylesheet" href="css/vendor/select2.min.css" />
+      <link rel="stylesheet" href="css/vendor/perfect-scrollbar.css" />
+      <link rel="stylesheet" href="css/vendor/glide.core.min.css" />
+      <link rel="stylesheet" href="css/vendor/bootstrap-stars.css" />
+      <link rel="stylesheet" href="css/vendor/nouislider.min.css" />
+      <link rel="stylesheet" href="css/vendor/bootstrap-datepicker3.min.css" />
+      <link rel="stylesheet" href="css/vendor/component-custom-switch.min.css" />
+      <link rel="stylesheet" href="css/main.css" />
+   </head>
+  <body>
+        @yield('content')
+  </body>
+</html>
